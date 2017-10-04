77f22fa3ce48ca2e2b3ec05236771bdd5bd59e70
Ethan Clark <elc3@students.calvin.edu>
2017-10-03 22:35:38 -0400

    Messed around with prototype a little bit

--- scraper_prototype.py
+++ scraper_prototype.py
@@ -4,6 +4,9 @@ from bs4 import BeautifulSoup
 calvin_page = 'http://calvinknights.com/sports/msoc/2017-18/teams/calvin?view=profile&r=0&pos=kickers'
 
 html_page = urllib2.urlopen(calvin_page)
-soup = BeautifulSoup(html_page)
+soup = BeautifulSoup(html_page, 'html.parser')
 table = soup.findAll("div", { "class" : "scrollable"})
 print(table)
+
+#table = soup.findAll("td", { "class" : "text pinned-col"})
+#print(table)
