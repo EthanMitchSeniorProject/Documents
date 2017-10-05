cfa67b26db6a0c1180c0db015b91850a4cc6853a
MitchStark10 <MitchStark10@gmail.com>
2017-10-05 09:34:13 -0400

    retrieve season statistics, not game by game

--- scraper_prototype.py
+++ scraper_prototype.py
@@ -6,8 +6,11 @@ calvin_page = 'http://calvinknights.com/sports/msoc/2017-18/teams/calvin?view=pr
 html_page = urllib2.urlopen(calvin_page)
 soup = BeautifulSoup(html_page, "html.parser")
 #Only search through the main statistics table, this query is more selective so we get fewer results
-stats_box = soup.find( "div", { "class" : "stats-box stats-box-alternate full clearfix"})
+tab_panel = soup.find("div", { "class" : "tab-panel clearfix active "})
+stats_box = tab_panel.find( "div", { "class" : "stats-box stats-box-alternate full clearfix"})
 table = stats_box.find("table")
 for element in table:
     href = element.find("a")
     #if href contains "players" it is a player stat table, need to parse the data
+    if "players" in str(href):
+        print(element)
